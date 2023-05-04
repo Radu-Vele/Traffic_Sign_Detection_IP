21cf963c3339d52c79eb97a6739320a0b5414040
radu_vele <99049451+Radu-Vele@users.noreply.github.com>
2023-05-04 15:44:05 +0300

    testing adaptive thresholding on the saturn image

--- OpenCVApplication.cpp
+++ OpenCVApplication.cpp
@@ -53,41 +53,26 @@ double computeConvolution(Mat src, int i, int j, double* kernel) {
 }
 
 bool isMaxOnDir(int curr_row, int curr_col, Mat magnitude, int angle) {
-	int curr_magnitude = magnitude.at<int>(curr_row, curr_col);
+	int curr_magnitude = magnitude.at<float>(curr_row, curr_col);
 	if (angle < 0) {
 		angle += 360;
 	}
 	if ((angle >= 22 && angle < 67) || (angle >= 202 && angle < 247)) { // 1
-		return curr_magnitude >= magnitude.at<int>(curr_row + 1, curr_col - 1) &&
-			(curr_magnitude >= magnitude.at<int>(curr_row - 1, curr_col + 1));
+		return curr_magnitude >= magnitude.at<float>(curr_row + 1, curr_col - 1) &&
+			(curr_magnitude >= magnitude.at<float>(curr_row - 1, curr_col + 1));
 	}
 	else if ((angle >= 67 && angle < 112) || (angle >= 247 && angle < 292)) { // 0
-		return curr_magnitude >= magnitude.at<int>(curr_row - 1, curr_col) &&
-			(curr_magnitude >= magnitude.at<int>(curr_row + 1, curr_col));
+		return curr_magnitude >= magnitude.at<float>(curr_row - 1, curr_col) &&
+			(curr_magnitude >= magnitude.at<float>(curr_row + 1, curr_col));
 	}
 	else if ((angle >= 112 && angle < 157) || (angle >= 292 && angle < 337)) { // 3
-		return curr_magnitude >= magnitude.at<int>(curr_row - 1, curr_col - 1) &&
-			(curr_magnitude >= magnitude.at<int>(curr_row + 1, curr_col + 1));
+		return curr_magnitude >= magnitude.at<float>(curr_row - 1, curr_col - 1) &&
+			(curr_magnitude >= magnitude.at<float>(curr_row + 1, curr_col + 1));
 	}
 	else { // 2
-		return curr_magnitude >= magnitude.at<int>(curr_row, curr_col - 1) &&
-			(curr_magnitude >= magnitude.at<int>(curr_row, curr_col + 1));
+		return curr_magnitude >= magnitude.at<float>(curr_row, curr_col - 1) &&
+			(curr_magnitude >= magnitude.at<float>(curr_row, curr_col + 1));
 	}
-}
-
-Mat scaleMatrix(Mat src, float scaling_factor) {
-	int rows = src.rows;
-	int cols = src.cols;
-	Mat result(rows, cols, CV_8UC1);
-
-	for (int i = 1; i < rows - 1; i++) {
-		for (int j = 1; j < cols - 1; j++) {
-			int curr_val = src.at<int>(i, j);
-			result.at<uchar>(i, j) = (uchar)((float)curr_val / scaling_factor);
-		}
-	}
-
-	return result;
 }
 
 int* computeHistogram(Mat src) {
@@ -102,22 +87,48 @@ int* computeHistogram(Mat src) {
 
 	for (int i = 1; i < rows - 1; i++) {
 		for (int j = 1; j < cols - 1; j++) {
-			histogram[src.at<uchar>(i, j)]++;
+			float pixel_value = src.at<float>(i, j);
+			int index = (int) pixel_value;
+			histogram[index]++;
 		}
 	}
 
 	return histogram;
 }
 
+void showHistogram(const std::string& name, int* hist, const int  hist_cols, const int hist_height)
+{
+	Mat imgHist(hist_height, hist_cols, CV_8UC3, CV_RGB(255, 255, 255)); // constructs a white image
+
+	//computes histogram maximum
+	int max_hist = 0;
+	for (int i = 0; i < hist_cols; i++)
+		if (hist[i] > max_hist)
+			max_hist = hist[i];
+	double scale = 1.0;
+	scale = (double)hist_height / max_hist;
+	int baseline = hist_height - 1;
+
+	for (int x = 0; x < hist_cols; x++) {
+		Point p1 = Point(x, baseline);
+		Point p2 = Point(x, baseline - cvRound(hist[x] * scale));
+		line(imgHist, p1, p2, CV_RGB(255, 0, 255)); // histogram bins colored in magenta
+	}
+
+	imshow(name, imgHist);
+}
+
 Mat cannyEdgeDetection(Mat src) {
 	int rows = src.rows;
 	int cols = src.cols;
 
-	Mat magnitude(rows, cols, CV_32SC1); // buffer for computed magnitude
-	Mat angle(rows, cols, CV_32SC1); // buffer for computed angles
-	Mat magnitude_max(rows, cols, CV_32SC1, Scalar(0)); // buffer for the maximum magnitudes on edge
+	Mat magnitude(rows, cols, CV_32FC1); // buffer for computed magnitude
+	Mat angle(rows, cols, CV_32FC1); // buffer for computed angles
+	Mat magnitude_max(rows, cols, CV_32FC1, Scalar(0)); // buffer for the maximum magnitudes on edge
 	Mat scaled_magnitude_max;
-	Mat scaled_magnitude_max_adaptive;
+	Mat scaled_magnitude_max_adaptive(rows, cols, CV_8UC1, Scalar(0));
+	
+	//blur input image
 	Mat src_blurred = gaussianBlur(src);
 	imshow("Blurred", src_blurred);	
 
@@ -129,46 +140,52 @@ Mat cannyEdgeDetection(Mat src) {
 		for (int j = 1; j < cols - 1; j++) {
 			double delta_x = computeConvolution(src_blurred, i, j, sobel_x);
 			double delta_y = computeConvolution(src_blurred, i, j, sobel_y);
-			magnitude.at<int>(i, j) = (int) sqrt(pow(delta_x, 2) + pow(delta_y, 2));
-			angle.at<int>(i, j) = (int)(atan2(delta_y, delta_x) * (180.0 / PI));
+			magnitude.at<float>(i, j) = (float) sqrt(pow(delta_x, 2) + pow(delta_y, 2));
+			angle.at<float>(i, j) = (float)(atan2(delta_y, delta_x) * (180.0 / PI));
 		}
 	}
 
+	// DEBUG
+	Mat magn_normalized(rows, cols, CV_8UC1);
+	convertScaleAbs(magnitude, magn_normalized);
+
+	imshow("Normalized Magnitude", magn_normalized);
+	//---
+
+	
 	//Non-maxima suppression
 	for (int i = 1; i < rows - 1; i++) {
 		for (int j = 1; j < cols -1; j++) {
-			if (isMaxOnDir(i, j, magnitude, angle.at<int>(i, j))) {
-				magnitude_max.at<int>(i, j) = magnitude.at<int>(i, j);
+			if (isMaxOnDir(i, j, magnitude, angle.at<float>(i, j))) {
+				magnitude_max.at<float>(i, j) = magnitude.at<float>(i, j);
 			}
 		}
 	}
 
+
 	//Adaptive thresholding
-	float sobel_scaling_factor = 1 / (4 * sqrt(2));
-	scaled_magnitude_max = scaleMatrix(magnitude_max, sobel_scaling_factor);
+	float sobel_scaling_factor = 1.0 / (4.0 * sqrt(2));
+	scaled_magnitude_max = sobel_scaling_factor * magnitude_max;
+
 	int* magnitude_histogram_scaled = computeHistogram(scaled_magnitude_max);
 
-	float p_value = 0.06; // set value between 0.01 and 0.1
-	int nr_no_edge_pixels = (int) ((1 - p_value) * (float)((rows - 1) * (cols - 1) - magnitude_histogram_scaled[0]));
-
-	printf("no edge pix %d\n", nr_no_edge_pixels);
+	showHistogram("hist", magnitude_histogram_scaled, 255, 1000);
+	float p_value = 0.1; // set value between 0.01 and 0.1
+	int nr_no_edge_pixels = (int)((1 - p_value) * (float)((rows - 2) * (cols - 2) - magnitude_histogram_scaled[0]));
 
 	int hist_count = 0;
 	int adaptive_threshold = 0;
 	for (adaptive_threshold = 1; adaptive_threshold < 255; adaptive_threshold++) {
+		hist_count += magnitude_histogram_scaled[adaptive_threshold];
 		if (hist_count >= nr_no_edge_pixels) {
 			break;
 		}
-		hist_count += magnitude_histogram_scaled[adaptive_threshold];
 	}
 
-	printf("th %d\n", adaptive_threshold);
-
-	scaled_magnitude_max_adaptive = scaled_magnitude_max.clone();
 	for (int i = 1; i < rows - 1; i++) {
 		for (int j = 1; j < cols - 1; j++) {
-			if (scaled_magnitude_max.at<uchar>(i, j) < adaptive_threshold) {
-				scaled_magnitude_max_adaptive.at<uchar>(i, j) = 0;
+			if (magnitude_max.at<float>(i, j) > adaptive_threshold) { // or scaled_magnitude_max?
+				scaled_magnitude_max_adaptive.at<uchar>(i, j) = 255;
 			}
 		}
 	}
@@ -180,8 +197,9 @@ Mat cannyEdgeDetection(Mat src) {
 }
 
 void processInput() {
-	Mat input_color = imread("./Images/harbor.bmp", IMREAD_COLOR);
-	Mat input_gray = colorToGrayscale(input_color);
+	/*Mat input_color = imread("./Images/harbor.bmp", IMREAD_COLOR);
+	Mat input_gray = colorToGrayscale(input_color);*/
+	Mat input_gray = imread("./Images/saturn.bmp", IMREAD_GRAYSCALE);
 	imshow("Input Gray", input_gray);
 	cannyEdgeDetection(input_gray);
 	waitKey(0);
